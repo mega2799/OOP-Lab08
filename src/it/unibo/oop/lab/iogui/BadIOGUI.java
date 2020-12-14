a4206bfecd2b3debbbc47bf6dbc237c7205926b3
mega2799 <megdigimatt@gmail.com>
2020-12-14 16:24:37 +0100

    Used lambda expr. but still couldnt find a good way to read from files, used a deprecated method without looking at solutions

--- src/it/unibo/oop/lab/iogui/BadIOGUI.java
+++ src/it/unibo/oop/lab/iogui/BadIOGUI.java
@@ -3,24 +3,15 @@ package it.unibo.oop.lab.iogui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.EOFException;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.InputStream;
-import java.io.OutputStream;
+import java.util.List;
 import java.util.Random;
 
-import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -39,8 +30,7 @@ import javax.swing.JPanel;
 public class BadIOGUI {
 
     private static final String TITLE = "A very simple GUI application";
-    private static final String PATH = System.getProperty("user.home")
-            + System.getProperty("file.separator")
+    private static final String PATH = System.getProperty("user.home") + System.getProperty("file.separator")
             + BadIOGUI.class.getSimpleName() + ".txt";
     private static final int PROPORTION = 5;
     private final Random rng = new Random();
@@ -50,93 +40,72 @@ public class BadIOGUI {
      * 
      */
     public BadIOGUI() {
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
         final JPanel windows = new JPanel();
-        windows.setLayout(new BoxLayout(windows, BoxLayout.Y_AXIS));
+        windows.setLayout(new BoxLayout(windows, BoxLayout.LINE_AXIS));
         final JButton write = new JButton("Write on file");
-        windows.add(write, BorderLayout.CENTER);
+        windows.add(write);
         final JButton read = new JButton("Read the file");
-        windows.add(read, BorderLayout.CENTER);
-        frame.setContentPane(windows);
+        final JButton space = new JButton("          "); // wanted a space beetween the buttons to look better
+        windows.add(space);
+        windows.add(read);
+        panel.add(windows, BorderLayout.CENTER);
+        frame.setContentPane(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  /*
+        /*
          * 
          * Handlers
          */
-       /* write.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-               try (OutputStream out = new FileOutputStream(PATH)) {
-                	final DataOutputStream stream = new DataOutputStream(out);
-                	stream.writeInt(rng.nextInt());
-                 } catch (FileNotFoundException e1) {
-                    JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
-                    e1.printStackTrace();
-                } catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-            }
-        });*/
-        write.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                try (PrintStream ps = new PrintStream(PATH)) {
-                    ps.print(rng.nextInt());
-                } catch (FileNotFoundException e1) {
-                    JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
-                    e1.printStackTrace();
-                }
+        write.addActionListener(e -> {
+            try (PrintStream ps = new PrintStream(PATH)) {
+                ps.print(rng.nextInt());
+            } catch (FileNotFoundException e1) {
+                JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
+                e1.printStackTrace();
             }
         });
+
+        read.addActionListener(e -> {
+            try (InputStream in = new FileInputStream(PATH)) {
+                DataInputStream data = new DataInputStream(in);
+                System.out.println(data.readLine());
+            } catch (FileNotFoundException e1) {
+                JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
+                e1.printStackTrace();
+            }
+        });
+
+        // Took from solution, readLine is deprecated and i didn't find a good alternative
         /*
-     read.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                try (InputStream in = new FileInputStream(PATH)){
-                	DataInputStream data = new DataInputStream(in);
-                	System.out.println(data.readInt());
-                }catch (FileNotFoundException e1) {
-                    JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
-                    e1.printStackTrace();
-                } catch (IOException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-		}
-            }
-        });*/
-read.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                try (final InputStream in = new FileInputStream(PATH)){
-                	final DataInputStream data = new DataInputStream(in);
-                	System.out.println(data.readLine());
-                }catch (FileNotFoundException e1) {
-                    JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
-                    e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-            }
-        });
- }
+         * read.addActionListener(e -> { try { List<String> lines =
+         * Files.readAllLines(new File(PATH).toPath()); for(final String l: lines) {
+         * System.out.println(l); } }catch (IOException e1) {
+         * JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
+         * e1.printStackTrace(); } });
+         */
+    }
 
     private void display() {
         /*
          * Make the frame one fifth the resolution of the screen. This very method is
-         * enough for a single screen setup. In case of multiple monitors, the
-         * primary is selected. In order to deal coherently with multimonitor
-         * setups, other facilities exist (see the Java documentation about this
-         * issue). It is MUCH better than manually specify the size of a window
-         * in pixel: it takes into account the current resolution.
+         * enough for a single screen setup. In case of multiple monitors, the primary
+         * is selected. In order to deal coherently with multimonitor setups, other
+         * facilities exist (see the Java documentation about this issue). It is MUCH
+         * better than manually specify the size of a window in pixel: it takes into
+         * account the current resolution.
          */
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         final int sw = (int) screen.getWidth();
         final int sh = (int) screen.getHeight();
         frame.setSize(sw / PROPORTION, sh / PROPORTION);
         /*
-         * Instead of appearing at (0,0), upper left corner of the screen, this
-         * flag makes the OS window manager take care of the default positioning
-         * on screen. Results may vary, but it is generally the best choice.
+         * Instead of appearing at (0,0), upper left corner of the screen, this flag
+         * makes the OS window manager take care of the default positioning on screen.
+         * Results may vary, but it is generally the best choice.
          */
         frame.setLocationByPlatform(true);
         /*
@@ -146,9 +115,10 @@ read.addActionListener(new ActionListener() {
     }
 
     /**
-     * @param args ignored
+     * @param args
+     *                 ignored
      */
     public static void main(final String... args) {
-       new BadIOGUI().display();
+        new BadIOGUI().display();
     }
 }
